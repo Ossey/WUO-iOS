1667369ad0fa96682dc642b64e438a3fb005e587
Ossey <xiaoyuan1314@me.com>
2017-01-15 19:57:22 +0800

    信息

---
+++ WUO/WUO/Classes/Message/ChatViewController.h
@@ -0,0 +1,22 @@
+//
+//  ChatViewController.h
+//  WUO
+//
+//  Created by mofeini on 17/1/14.
+//  Copyright © 2017年 com.test.demo. All rights reserved.
+//
+
+#import "EaseMessageViewController.h"
+
+#define KNOTIFICATIONNAME_DELETEALLMESSAGE @"RemoveAllMessages"
+
+@class XYUserInfo;
+@interface ChatViewController : EaseMessageViewController <EaseMessageViewControllerDelegate, EaseMessageViewControllerDataSource>
+
+@property (nonatomic, strong) XYUserInfo *user;
+
+- (void)showMenuViewController:(UIView *)showInView
+                  andIndexPath:(NSIndexPath *)indexPath
+                   messageType:(EMMessageBodyType)messageType;
+
+@end
