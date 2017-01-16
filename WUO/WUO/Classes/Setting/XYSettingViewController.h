9f07b5b2038b49d773accf95f5bb7b9f57dd99f8
Ossey <xiaoyuan1314@me.com>
2017-01-16 19:31:04 +0800

    集成友盟，使用第三方QQ登录

---
+++ WUO/WUO/Classes/Setting/XYSettingViewController.h
@@ -0,0 +1,13 @@
+//
+//  XYSettingViewController.h
+//  WUO
+//
+//  Created by mofeini on 17/1/16.
+//  Copyright © 2017年 com.test.demo. All rights reserved.
+//
+
+#import "XYProfileBaseController.h"
+
+@interface XYSettingViewController : XYProfileBaseController
+
+@end
